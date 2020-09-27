5e53998b9e27baeafa43a1a3870e8cb45fb187f5
Developer0007 <ajamadar1590@gmail.com>
2020-09-27 21:33:19 +0530

    EXP-05

---
+++ EXP-5/AJAMADAR/footer.php
@@ -0,0 +1,5 @@
+        <footer id="footer">
+                <small>&copy; Copyright 2020, CAR RENTAL</small> 
+        </footer>
+    </body>
+</html>
---
+++ EXP-5/AJAMADAR/index.php
@@ -0,0 +1,32 @@
+<?php
+include('head.php');
+include('nav.php');
+?>
+<div id = "home">
+    <img src="unplash.jpg" class="img-fluid" alt="Responsive image">
+</div>
+<div id="about">
+<div class="container">
+
+  <div class="row">
+    <div class="col-8"><img src="avatar.png" class="img-thumbnail" width=250px height=250px alt=""></div>
+    <div class="col-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque temporibus animi cupiditate earum ducimus ipsam dicta eveniet magnam sunt laudantium quam tenetur, perferendis quis distinctio sed ex? Eligendi, expedita facilis?</div>
+  </div>
+  
+</div>
+<div class="container">
+  <div class="row">
+  <div class="col-8">Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque temporibus animi cupiditate earum ducimus ipsam dicta eveniet magnam sunt laudantium quam tenetur, perferendis quis distinctio sed ex? Eligendi, expedita facilis?</div>  
+  <div class="col-4"><img src="avatar.png" class="img-thumbnail" width=250px height=250px alt=""></div>
+  </div>
+</div>
+</div>
+<div class="container" id="contact">
+<address>
+    <strong>Car Rental, Inc.</strong><br>
+    1355 Market St, Suite 900<br>
+    San Francisco, CA 94103<br>
+    <abbr title="Phone">P:</abbr> (123) 456-7890
+</address>
+</div>
+<?php include('footer.php'); ?>
